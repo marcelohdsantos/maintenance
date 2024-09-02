ad0da162d09f469821b0c6bb012f62399a06a2d1
Marcelo Henrique dos Santos (BR-ICCT) <marcelo_santos@calcomp-icct.org.br>
2024-09-02 10:36:06 -0400

    realizado alteração utilizar dicionário.

--- app.py
+++ app.py
@@ -20,18 +20,23 @@ db_config = {
 
 if __name__ == '__main__':
     # mapear os diretórios para filtrar
+    # diretorio_filtros = {
+    #     r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S1',
+    #     r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S2',
+    #     r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S3',
+    #     r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S4',
+    #     r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S5',
+    #     r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S6',
+    #     r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S7',
+    # }
+
     diretorio_filtros = {
-        # r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S1',
-        # r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S2',
-        # r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S3',
-        # r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S4',
-        # r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S5',
-        # r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S6',
-        # r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': 'S7',
+        r'\\10.58.141.25\\d\\setup-lineS5\\iotsoftware\\SMT_Center_LogAnalyzer\\maintenanceCommand.txt': ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7']
     }
 
     # verificar e normalizar os caminhos dos arquivos
     normalized_diretorios_filtros = {}
+
     for path, filtro in diretorio_filtros.items():
         if isinstance(path, (str, bytes, os.PathLike)) and path:
             normalized_path = os.path.normpath(path)
@@ -45,25 +50,30 @@ if __name__ == '__main__':
         if not isinstance(path, (str, bytes, os.PathLike)) or not path:
             raise ValueError(f"O caminho '{path}' não é valido.")
 
-    # Processar os arquivos inicialmente
-    processar_inicialmente(diretorio_filtros, db_config)
+    for path, filtros in diretorio_filtros.items():
+        for filtro in filtros:
+            # Processar os arquivos inicialmente
+            processar_inicialmente({path: filtro}, db_config)
 
     padroes = ["*maintenanceCommand.txt"]
     observers = []
 
-    for diretorio in set(os.path.dirname(arquivo) for arquivo in diretorio_filtros.keys()):
-        try:
-            print(f"[DEBUG] Configurando monitoramento para o diretório: {
-                  diretorio}")
-            event_handler = EventHandler(padroes, diretorio_filtros, db_config)
-            observer = Observer()
-            observer.schedule(event_handler, path=diretorio, recursive=False)
-            observers.append(observer)
-            print(f"[DEBUG] Monitoramento iniciado para o diretório: {
-                  diretorio}")
-        except Exception as e:
-            print(f"[ERROR] Erro ao configurar o monitoramento para o diretório {
-                  diretorio}: {e}")
+    for path, filtros in diretorio_filtros.items():
+        for filtro in filtros:
+            try:
+                print(f"[DEBUG] Configurando monitoramento para o diretório: {
+                      os.path.dirname(path)} com o filtro: {filtro}")
+                event_handler = EventHandler(
+                    padroes, {path: filtro}, db_config)
+                observer = Observer()
+                observer.schedule(
+                    event_handler, path=os.path.dirname(path), recursive=False)
+                observers.append(observer)
+                print(f"[DEBUG] Monitoramento iniciado para o diretório: {
+                      os.path.dirname(path)} com o filtro: {filtro}")
+            except Exception as e:
+                print(f"[ERROR] Erro ao configurar o monitoramento para o diretório {
+                      os.path.dirname(path)} com o filtro: {filtro}: {e}")
 
     try:
         for observer in observers:
